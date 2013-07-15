bf64feb081ecde55215a3819cfe19f3296106bf7
zs <baloghzsof@gmail.com>
2013-07-15 12:07:32 +0200

    BranchExecutioner.

--- lib/git_brd.rb
+++ lib/git_brd.rb
@@ -1,16 +1,19 @@
 require "git_brd/version"
-require 'logger'
 require 'git'
 
 module GitBrd
   class BranchExecutioner
   	def initialize(regex_str)
-  		@regex 	= /"#{regex_str}"/
-  		@git 		= Git.open(Dir.pwd, :log => ::Logger.new(STDOUT))
+  		@regex 	= /#{regex_str}/
+  		@git 		= Git.open(Dir.pwd)
   	end
 
   	def matching_branches
-  		@git.branches.local
+  		@matching_branches ||= @git.branches.local.select{ |branch| branch.name =~ @regex }
+  	end
+
+  	def delete_matching_branches!
+  		matching_branches.each &:delete
   	end
   end
 end
